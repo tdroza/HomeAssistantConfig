4d7d8cc55824fd3758bafc605cee5b5fffb06dae
Tom D'Roza <724245+tdroza@users.noreply.github.com>
2020-02-19 10:54:19 +0000

    Added: - GlobalVariables.js for theme independent colours (currently used for temperature thresholds in mini-graph-card)

---
+++ www/GlobalVariables.js
@@ -0,0 +1,6 @@
+document.documentElement.style.setProperty('--humidity-color', '#56a0ee');
+document.documentElement.style.setProperty('--temp-twenty-color', '#c0392b');
+document.documentElement.style.setProperty('--temp-nineteen-color', '#d35400');
+document.documentElement.style.setProperty('--temp-eighteen-color', '#f39c12'); 
+document.documentElement.style.setProperty('--temp-seventeen-color', '#03a9f4');
+document.documentElement.style.setProperty('--temp-sixteen-color', '#0000cc');
