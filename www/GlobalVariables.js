ae5122ef4290a21ec52046cdfeba81131d3854c4
Tom D'Roza <724245+tdroza@users.noreply.github.com>
2020-03-01 11:10:16 +0000

    Changed: - Names of color variables Added: - Lovelace tab for PiHole stats

--- www/GlobalVariables.js
+++ www/GlobalVariables.js
@@ -1,6 +1,6 @@
-document.documentElement.style.setProperty('--humidity-color', '#56a0ee');
-document.documentElement.style.setProperty('--temp-twenty-color', '#c0392b');
-document.documentElement.style.setProperty('--temp-nineteen-color', '#d35400');
-document.documentElement.style.setProperty('--temp-eighteen-color', '#f39c12'); 
-document.documentElement.style.setProperty('--temp-seventeen-color', '#03a9f4');
-document.documentElement.style.setProperty('--temp-sixteen-color', '#0000cc');
+document.documentElement.style.setProperty('--humidity', '#56a0ee');
+document.documentElement.style.setProperty('--temp-twenty', '#c0392b');
+document.documentElement.style.setProperty('--temp-nineteen', '#d35400');
+document.documentElement.style.setProperty('--temp-eighteen', '#f39c12'); 
+document.documentElement.style.setProperty('--temp-seventeen', '#03a9f4');
+document.documentElement.style.setProperty('--temp-sixteen', '#0000cc');
