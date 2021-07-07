8b25b12bd2c7272beb4ac86dba5142c54d047ead
Tom D'Roza <724245+tdroza@users.noreply.github.com>
2021-07-07 17:25:18 +0100

    Added: min_max sensors to record minimum and maximum recorded temperatures from all temp sensors Added: Dynamic colouring of graphs now works using the min/max/mean as the scale for colours Added: Colours values now come from input_text helpers Removed: Old colour values removed from GlobalVariables (didn't work reliably)

--- www/GlobalVariables.js
+++ www/GlobalVariables.js
@@ -1,6 +1,1 @@
 document.documentElement.style.setProperty('--humidity', '#56a0ee');
-document.documentElement.style.setProperty('--temp-twenty', '#c0392b');
-document.documentElement.style.setProperty('--temp-nineteen', '#d35400');
-document.documentElement.style.setProperty('--temp-eighteen', '#f39c12'); 
-document.documentElement.style.setProperty('--temp-seventeen', '#03a9f4');
-document.documentElement.style.setProperty('--temp-sixteen', '#0000cc');
